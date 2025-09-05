0ff1411e8b486b99fa7c265f53dab366fffc8984
hologutbiz <holoakinseloyin@gmail.com>
2025-09-05 12:54:38 +0100

    first commit since import project import to netlify

---
+++ scripts/classification-data.js
@@ -0,0 +1,41 @@
+
+
+// load chart data
+
+fetch("https://lotto-classification-api.netlify.app/.netlify/functions/classification")
+    .then(res => {
+        if(!res.ok) throw new Error(`fetch failded: ${res.status}`);
+
+        return res.json();
+    })
+    .then(data => {
+        if (!data || !data.one_to_fortyfive) throw new Error("No valid data received");
+
+        const fillTable = (dataset, tbodyId) => {
+            const tbody = document.getElementById(tbodyId);
+            dataset.forEach(num => {
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td class="num">${num.number}</td>
+                    <td>${num.counterpart}</td>
+                    <td>${num.bonanza}</td>
+                    <td>${num.malta}</td>
+                    <td>${num.stringkey}</td>
+                    <td>${num.shadow}</td>
+                    <td>${num.partner}</td>
+                    <td>${num.equivalent}</td>
+                    <td>${num.code}</td>
+                    <td>${num.turning}</td>
+                `;
+
+                row.appendChild(tbody);
+            })
+
+            fillTable(data.one_to_fortyfive, "oneTofortyfive");
+            fillTable(data.fortysix_to_ninety, "fortysixToNinety");
+        }
+    })
+    .catch (err => {
+        console.error(err);
+        }
+    )
