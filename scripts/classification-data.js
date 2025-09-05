9285328a2b2e791cc963c1611742bcb2f75a033b
hologutbiz <holoakinseloyin@gmail.com>
2025-09-05 13:57:26 +0100

    wrapped fetch logic inside window event lsitener

--- scripts/classification-data.js
+++ scripts/classification-data.js
@@ -1,43 +1,43 @@
 
 
 // load chart data
+window.addEventListener("DOMContentLoaded", () => {
+    fetch("https://lotto-classification-api.netlify.app/.netlify/functions/classification")
+        .then(res => {
+            if(!res.ok) throw new Error(`fetch failded: ${res.status}`);
+            return res.json();
+        })
+        .then(data => {
+            if (!data || !data.one_to_fortyfive) throw new Error("No valid data received");
 
-fetch("https://lotto-classification-api.netlify.app/.netlify/functions/classification")
-    .then(res => {
-        if(!res.ok) throw new Error(`fetch failded: ${res.status}`);
-        return res.json();
-    })
-    .then(data => {
-        if (!data || !data.one_to_fortyfive) throw new Error("No valid data received");
+            console.log("Fetched data: ", data);
 
-        console.log("Fetched data: ", data);
+            const fillTable = (dataset, tbodyId) => {
+                const tbody = document.getElementById(tbodyId);
+                dataset.forEach(num => {
+                    const row = document.createElement("tr");
+                    row.innerHTML = `
+                        <td class="num">${num.number}</td>
+                        <td>${num.counterpart}</td>
+                        <td>${num.bonanza}</td>
+                        <td>${num.malta}</td>
+                        <td>${num.stringkey}</td>
+                        <td>${num.shadow}</td>
+                        <td>${num.partner}</td>
+                        <td>${num.equivalent}</td>
+                        <td>${num.code}</td>
+                        <td>${num.turning}</td>
+                    `;
 
-        const fillTable = (dataset, tbodyId) => {
-            const tbody = document.getElementById(tbodyId);
-            dataset.forEach(num => {
-                const row = document.createElement("tr");
-                row.innerHTML = `
-                    <td class="num">${num.number}</td>
-                    <td>${num.counterpart}</td>
-                    <td>${num.bonanza}</td>
-                    <td>${num.malta}</td>
-                    <td>${num.stringkey}</td>
-                    <td>${num.shadow}</td>
-                    <td>${num.partner}</td>
-                    <td>${num.equivalent}</td>
-                    <td>${num.code}</td>
-                    <td>${num.turning}</td>
-                `;
+                    tbody.appendChild(row);
+                });
+            };
 
-                tbody.appendChild(row);
-            })
-        }
-
-        fillTable(data.one_to_fortyfive, "oneTofortyfive");
-        fillTable(data.fortysix_to_ninety, "fortysixToNinety");
-        console.log(fillTable);
-    })
-    .catch (err => {
-        console.error(err);
-        }
-    )
+            fillTable(data.one_to_fortyfive, "oneTofortyfive");
+            fillTable(data.fortysix_to_ninety, "fortysixToNinety");
+        })
+        .catch (err => {
+            console.error(err);
+            }
+        )
+});
