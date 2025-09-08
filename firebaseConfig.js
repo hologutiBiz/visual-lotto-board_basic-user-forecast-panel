5ca7f5b241163e7b2242f0e66bbd38a0a875de35
hologutbiz <holoakinseloyin@gmail.com>
2025-09-08 14:13:38 +0100

    integrate reCAPTCHA

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebas
 import { getAuth } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js'
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js'
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app-check.js";
 
 let auth, firestore;
 
@@ -9,7 +10,7 @@ export async function initFirebase(appName = "basicUserWebApp") {
     try {
         const res = await fetch(`https://firebase-config-key.netlify.app/.netlify/functions/htmlProject?app=${appName}`);
         if (!res.ok) throw new Error ("Failed to fetch firebase config");
-
+console.log("Raw config response:", res);
         const firebaseConfig = await res.json();
         if (!firebaseConfig.projectId) throw new Error("Missing project Id");
 
@@ -18,9 +19,15 @@ export async function initFirebase(appName = "basicUserWebApp") {
         firestore = getFirestore(app);
         const analytics = getAnalytics(app);
 
+        const appCheck = initializeAppCheck(app, {
+          provider: new ReCaptchaEnterpriseProvider("6Lf3PMIrAAAAAF1HCIAGNXi-oeHzyOz697irEW2a"),
+          isTokenAutoRefreshEnabled: true
+
+        });
+
         console.log("✅ Firebase initialized ");
   } catch (error) {
-    console.error("🔥 Firebase init failed:", error)
+     console.error("🔥 Firebase init failed:", error)
     throw error;
   }
 }
