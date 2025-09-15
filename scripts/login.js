8b14bbd9d561e85b5cae7438cd57e54390c813fb
hologutbiz <holoakinseloyin@gmail.com>
2025-09-15 16:37:55 +0100

    debugging login

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,4 @@
-import { Auth, firestoreDB } from "../firebaseConfig";
+import { Auth, firestoreDB } from "../firebaseConfig.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
