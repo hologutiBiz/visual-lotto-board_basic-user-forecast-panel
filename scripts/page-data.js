e12f68faa644e299e313b7692607a8966b7c473e
hologutbiz <holoakinseloyin@gmail.com>
2025-09-16 03:19:52 +0100

    implement user expiry date

--- scripts/page-data.js
+++ scripts/page-data.js
@@ -1,5 +1,6 @@
-import { initFirebase, Auth } from "../firebaseConfig.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { initFirebase, Auth, firestoreDB } from "../firebaseConfig.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import { fetchFrequentNumbers } from "./frequently-numbers.js";
 import { fetchClassificationChart } from "./classification-data.js"; 
 
@@ -13,6 +14,27 @@ document.addEventListener("DOMContentLoaded", async () => {
             if (!user) {
                 window.location.href = "index.html";
                 return;
+            }
+
+            const userRef = doc(firestoreDB(), "users", user.uid());
+            const userSnap = await getDoc(userRef);
+
+            if (!userSnap.exists()) {
+                await signOut(Auth());
+                window.location.href = "https://app.visuallottoboard.com/sign-up";
+                return;
+            }
+
+            const subEndDate = userSnap.data().subscriptionExpiry;
+            const subEnd = new Date(subEndDate);
+            const today = new Date();
+
+            const subExpired = subEnd < today;
+
+            if (subExpired) {
+                alert("Your subscription has expired. Please renew to continye.");
+                await signOut(Auth());
+                window.location.href = "https://app.visuallottoboard.com/subscription";
             }
 
             fetchFrequentNumbers();
